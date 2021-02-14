c28e0a5c6c5ebf6525bd928d4fde8e17fcf21766
Hekaiyo <sylviali21098@berkeley.edu>
2021-02-13 23:08:29 -0800

    Added basic click functionality

---
+++ Assets/Scripts/LeafManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafManager : MonoBehaviour
+{
+    public int leaves;
+    public int leavesPerPunch;
+    public GameObject leavesCounter;
+    private UnityEngine.UI.Text leavesCounterText;
+
+    public void Start()
+    {
+        leavesCounterText = leavesCounter.GetComponent<UnityEngine.UI.Text>();
+        updateLeafCount();
+    }
+
+    private void updateLeafCount()
+    {
+        if (leaves == 1)
+            leavesCounterText.text = "1 Leaf";
+        else
+            leavesCounterText.text = leaves + " Leaves";
+    }
+
+    public void punch()
+    {
+        leaves += leavesPerPunch;
+        updateLeafCount();
+    }
+}
